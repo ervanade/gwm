cf64f27fe92bb8fc7528c1d97ce1b72635cc35f5
Ervan Ade Wijaya <89782667+ervanade@users.noreply.github.com>
2025-09-01 17:47:11 +0700

    fix redirect

--- next.config.mjs
+++ next.config.mjs
@@ -4,36 +4,37 @@ const withNextIntl = createNextIntlPlugin();
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-      remotePatterns: [
-        {
-          protocol: "https",
-          hostname: "**",
-        },
-        {
-          protocol: "http",
-          hostname: "**",
-        },
-      ]
-    },
-    cacheMaxMemorySize: 104857600, // 100 MB dalam byte
-    headers: async () => {
-      return [
-        {
-          source: "/:path*",
-          headers: [
-            {
-              key: "Cache-Control",
-              value: "public, max-age=3600, immutable", // Long caching for static assets
-            }
-          ],
-        },
-      ];
-    },
-    reactStrictMode: true,  // Enables strict mode for better debugging
-    // swcMinify: true,  // Faster JavaScript minification
-    compress: true,   // Enables compression (gzip and brotli)
-    // optimizeFonts: true,  // Optimizes font loading for better performance
-  };
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+      {
+        protocol: "http",
+        hostname: "**",
+      },
+    ]
+  },
+  cacheMaxMemorySize: 104857600, // 100 MB dalam byte
+  headers: async () => {
+    return [
+      {
+        source: "/:path*",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public, max-age=3600, immutable", // Long caching for static assets
+          }
+        ],
+      },
+    ];
+  },
+  reactStrictMode: true,  // Enables strict mode for better debugging
+  // swcMinify: true,  // Faster JavaScript minification
+  compress: true,   // Enables compression (gzip and brotli),
+  trailingSlash: false
+  // optimizeFonts: true,  // Optimizes font loading for better performance
+};
 
 export default withNextIntl(nextConfig);
